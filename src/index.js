f8669041677b77162aa8ed065273de65c7eadaa9
dan.nguyen <dannd496@gmail.com>
2022-12-29 20:25:54 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { sequelize } = require("./models");
+const { AppError, handleErrors } = require("./helpers/error");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("."));
+
+// Sync cái model của sequelize với DB
+sequelize.sync({ alter: true });
+
+const v1 = require("./routers/v1");
+app.use("/api/v1", v1);
+
+// Demo handle error
+app.get("/error", (req, res, next) => {
+  throw new AppError(500, "Internal Server");
+});
+
+// Middleware dùng để bắt và xử lý trả lỗi ra cho client
+// Phải được đặt bên dưới các routers
+app.use(handleErrors);
+
+app.listen(4000);
---
+++ src/services/auth.service.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models");
+const { AppError } = require("../helpers/error");
+const { generateToken } = require("../helpers/jwt");
+
+const login = async (credentials) => {
+  try {
+    const { email, password } = credentials;
+    const user = await User.findOne({
+      where: { email },
+      attributes: { include: ["password"] },
+    });
+
+    if (!user) {
+      throw new AppError(400, "email or password invalid");
+    }
+
+    const isMatched = bcrypt.compareSync(password, user.password);
+    if (!isMatched) {
+      throw new AppError(400, "email or password invalid");
+    }
+
+    return generateToken(user);
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  login,
+};
